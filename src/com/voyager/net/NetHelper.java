f6d9c93c2198ae392524c44a86e78b08f17aad07
wuhaojie <w19961009@126.com>
2016-01-05 16:45:49 +0800

    test netHelper.

--- src/com/voyager/net/NetHelper.java
+++ src/com/voyager/net/NetHelper.java
@@ -1,5 +1,77 @@
 package com.voyager.net;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 public class NetHelper {
+
+	public static void post() {
+		// 创建HttpClient实例对象
+		CloseableHttpClient httpclient = HttpClients.createDefault();
+		// 创建Post请求
+		HttpPost httppost = new HttpPost(
+				"http://localhost:8080/服务端/servlet/Login");
+		// 创建参数列表
+		List<BasicNameValuePair> formparams = new ArrayList<BasicNameValuePair>();
+		formparams.add(new BasicNameValuePair("user", "lisi"));
+		formparams.add(new BasicNameValuePair("pwd", "123456"));
+		UrlEncodedFormEntity uefEntity;
+		try {
+			uefEntity = new UrlEncodedFormEntity(formparams, "UTF-8");
+			// 设置廉洁配置
+			httppost.setEntity(uefEntity);
+			System.out.println("executing request " + httppost.getURI());
+			// 执行连接操作
+			CloseableHttpResponse response = httpclient.execute(httppost);
+			try {
+				HttpEntity entity = response.getEntity();
+				if (entity != null) {
+					Header[] allHeaders = response.getAllHeaders();
+					for (Header h : allHeaders) {
+						System.out
+								.println(h.getName() + "  :  " + h.getValue());
+					}
+					System.out
+							.println("--------------------------------------");
+					System.out.println("Response content: "
+							+ EntityUtils.toString(entity, "UTF-8"));
+					System.out
+							.println("--------------------------------------");
+				}
+			} finally {
+				response.close();
+			}
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			// 关闭连接,释放资源
+			try {
+				httpclient.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		post();
+	}
 
 }
